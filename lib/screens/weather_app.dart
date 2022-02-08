87a6d4633d3e1df93ae75a862f58516fb6e7e49f
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-02-08 12:16:21 +0100

    app completion

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -1,6 +1,15 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:myweather/models/weatehr_location.dart';
+import 'package:myweather/widget/single_weather.dart';
+
+import 'package:transformer_page_view/transformer_page_view.dart';
+
+import '../models/weatehr_location.dart';
+import '../widget/buildin_transform.dart';
+import '../widget/single_weather.dart';
+import '../widget/slider_dot.dart';
 
 class weatherApp extends StatefulWidget {
   const weatherApp({Key? key}) : super(key: key);
@@ -10,45 +19,79 @@ class weatherApp extends StatefulWidget {
 }
 
 class _weatherAppState extends State<weatherApp> {
+  int _currentPage = 0;
+  late String bgImg;
+
+  _onPageChanged(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (locationList[_currentPage].weatherType == 'Sunny') {
+      bgImg = 'assets/sunny.jpg';
+    } else if (locationList[_currentPage].weatherType == 'Night') {
+      bgImg = 'assets/night.jpg';
+    } else if (locationList[_currentPage].weatherType == 'Rainy') {
+      bgImg = 'assets/rainy.jpg';
+    } else if (locationList[_currentPage].weatherType == 'Cloudy') {
+      bgImg = 'assets/cloudy.jpeg';
+    }
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar:AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.search, size:30, color:Colors.white), onPressed: () {  },
+          icon: Icon(Icons.search, size: 30, color: Colors.white),
+          onPressed: () {},
         ),
         actions: [
           Container(
             margin: EdgeInsets.only(right: 20),
             child: GestureDetector(
-                onTap: ()=>print('Menu Clicked'),
-                child: Icon(Icons.menu, size: 30, color: Colors.white,)),
+              onTap: () => print('Menu Clicked'),
+              child: SvgPicture.asset(
+                "assets/menu.svg",
+                width: 30,
+                height: 30,
+                color: Colors.white,
+              ),
+            ),
           )
         ],
       ),
       body: Container(
         child: Stack(
           children: [
-            Image.asset("assets/night.jpg",fit: BoxFit.cover,height: double.infinity, width: double.infinity ,),
+            Image.asset(
+              bgImg,
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+            ),
             Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
+              decoration: BoxDecoration(color: Colors.black38),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 140, left: 15),
+              child: Row(
                 children: [
-                  SizedBox(
-                    height: 120,
-                  ),
-                  Text("Koltkata", style: GoogleFonts.poppins(
-                      textStyle: const TextStyle(
-                          color: Colors.white,
-                          letterSpacing: 1,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal)),),
+                  for (int i = 0; i < locationList.length; i++)
+                    if (i == _currentPage) SliderDot(true) else SliderDot(false)
                 ],
               ),
-            )
+            ),
+            TransformerPageView(
+              scrollDirection: Axis.horizontal,
+              transformer: ScaleAndFadeTransformer(),
+              viewportFraction: 0.8,
+              onPageChanged: _onPageChanged,
+              itemCount: locationList.length,
+              itemBuilder: (ctx, i) => singleWeather(i),
+            ),
           ],
         ),
       ),
